e326d5c5b870df6c9a279a23f205060ee6dbc814
clayton sims <csims@dimagi.com>
2012-11-29 15:18:04 -0500

    Pause/Play functionality from audioButton

--- src/org/odk/collect/android/views/AudioButton.java
+++ src/org/odk/collect/android/views/AudioButton.java
@@ -4,13 +4,14 @@
 
 package org.odk.collect.android.views;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.javarosa.core.reference.InvalidReferenceException;
 import org.javarosa.core.reference.ReferenceManager;
 import org.odk.collect.android.R;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
 import android.util.Log;
@@ -18,9 +19,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 import android.widget.Toast;
-
-import java.io.File;
-import java.io.IOException;
 
 /**
  * @author ctsims
@@ -31,16 +29,21 @@ public class AudioButton extends ImageButton implements OnClickListener {
     private String URI;
     private MediaPlayer player;
 
+    enum ButtonState {
+    	Playing,
+    	Ready,
+    	Paused;
+    }
+    
+    ButtonState currentState;
 
     public AudioButton(Context context, String URI) {
         super(context);
         this.setOnClickListener(this);
         this.URI = URI;
-        Bitmap b =
-            BitmapFactory.decodeResource(context.getResources(),
-                android.R.drawable.ic_lock_silent_mode_off);
-        this.setImageBitmap(b);
+        this.setImageResource(R.drawable.ic_media_btn_play);
         player = null;
+        currentState = ButtonState.Ready;
     }
 
 
@@ -71,35 +74,48 @@ public class AudioButton extends ImageButton implements OnClickListener {
             Toast.makeText(getContext(), errorMsg, Toast.LENGTH_LONG).show();
             return;
         }
+        switch(currentState) {
+        case Ready:
+            player = new MediaPlayer();
+            try {
+                player.setDataSource(audioFilename);
+                player.prepare();
+                player.start();
+                player.setOnCompletionListener(new OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                    	stopPlaying();
+                    }
 
-        // In case we're currently playing sounds.
-        stopPlaying();
-
-        player = new MediaPlayer();
-        try {
-            player.setDataSource(audioFilename);
-            player.prepare();
-            player.start();
-            player.setOnCompletionListener(new OnCompletionListener() {
-                @Override
-                public void onCompletion(MediaPlayer mediaPlayer) {
-                    mediaPlayer.release();
-                }
-
-            });
-        } catch (IOException e) {
-            String errorMsg = getContext().getString(R.string.audio_file_invalid);
-            Log.e(t, errorMsg);
-            Toast.makeText(getContext(), errorMsg, Toast.LENGTH_LONG).show();
-            e.printStackTrace();
+                });
+                currentState = ButtonState.Playing;
+                this.setImageResource(R.drawable.ic_media_pause);
+            } catch (IOException e) {
+                String errorMsg = getContext().getString(R.string.audio_file_invalid);
+                Log.e(t, errorMsg);
+                Toast.makeText(getContext(), errorMsg, Toast.LENGTH_LONG).show();
+                e.printStackTrace();
+            }
+        	break;
+        case Paused:
+        	player.start();
+        	currentState = ButtonState.Playing;
+        	this.setImageResource(R.drawable.ic_media_pause);
+        	break;
+        case Playing:
+        	player.pause();
+        	currentState = ButtonState.Paused;
+        	this.setImageResource(R.drawable.ic_media_btn_continue);
+        	break;
         }
-
     }
 
 
     public void stopPlaying() {
         if (player != null) {
             player.release();
+            currentState = ButtonState.Ready;
+            this.setImageResource(R.drawable.ic_media_btn_play);
         }
     }
 }
