68e7323178ef79759e4fd75e05b1b4b3ff7453ea
Jenny Schweers <jschweers@dimagi.com>
2014-07-21 08:50:06 -0400

    PR #64: bug fix for 'Application crashes when played audio files in 'Multimedia demo' form.'

--- src/org/odk/collect/android/views/media/AudioButton.java
+++ src/org/odk/collect/android/views/media/AudioButton.java
@@ -309,8 +309,10 @@ public class AudioButton extends ImageButton implements OnClickListener {
     }
 
     public void endPlaying() {
-    	controller.releaseCurrentMediaEntity();
-    	setStateToReady();
+		if (!currentState.equals(MediaState.Playing)) {
+    		controller.releaseCurrentMediaEntity();
+    		setStateToReady();
+		}
     }
 
     public void pausePlaying() {
