5b6a51a32a40ffae31628a6c17c2b7431332b11f
ALIZA STONE <amstone326@gmail.com>
2014-08-01 10:14:26 -0400

    added class-level comment and changed source to source.substring(start,end)

--- src/org/odk/collect/android/utilities/IntegerSizeFilter.java
+++ src/org/odk/collect/android/utilities/IntegerSizeFilter.java
@@ -3,6 +3,15 @@ package org.odk.collect.android.utilities;
 import android.text.InputFilter;
 import android.text.Spanned;
 
+/**
+ * @author amstone326
+ * 
+ * An InputFilter that limits the characters a user can enter,
+ * based on whether or not they represent a valid integer in Java.
+ * Used by IntegerWidget.java.
+ *
+ */
+
 public class IntegerSizeFilter implements InputFilter {
 
 	@Override
@@ -14,7 +23,7 @@ public class IntegerSizeFilter implements InputFilter {
 		}
 		String part1 = destString.substring(0, dstart);
 		String part2 = destString.substring(dend);
-		String newString = part1 + (String)source + part2;
+		String newString = part1 + (String)source.subSequence(start, end) + part2;
 		
 		try {
 			Integer x = Integer.parseInt((String)newString);
